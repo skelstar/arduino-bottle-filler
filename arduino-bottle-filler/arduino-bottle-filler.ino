0e75bbb90afdb12aa06b9215c91ff95fe92b1a7d
skelstar <skelstar@gmail.com>
2017-01-14 14:26:22 +1300

    Working with a button (myPushButton)

--- arduino-bottle-filler/arduino-bottle-filler.ino
+++ arduino-bottle-filler/arduino-bottle-filler.ino
@@ -1,8 +1,8 @@
 
 #include "paj7620.h"
 #include <myWifiHelper.h>
-
 #include <Adafruit_NeoPixel.h>
+#include <myPushButton.h>
 
 /* ----------------------------------------------------------- */
 
@@ -10,18 +10,23 @@
 #define PIN            15
 #define NUMPIXELS      12
 
-#define SUCCESSFUL_CONNECT  1
+//#define SUCCESSFUL_CONNECT  1
 
-int head = 4;
-#define TRAIL_LENGTH    6
-int brightnesses[] = {140,125,75,30,10,5,0,0,0,0,0,0};
-bool lightsOn = false;
+//int head = 4;
+//#define TRAIL_LENGTH    6
+//int brightnesses[] = {140,125,75,30,10,5,0,0,0,0,0,0};
+//bool lightsOn = false;
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ400);
 
-int delayval = 400;
-unsigned long timer;
+//int delayval = 400;
+//unsigned long timer;
 
+#define BUTTON_PIN  14
+
+void listener_Button(int eventCode, int eventParams);
+
+myPushButton button(BUTTON_PIN, true, 2000, 1, listener_Button);
 
 /* ----------------------------------------------------------- */
 void wifiMsgCallback(char* message);
@@ -40,8 +45,8 @@ void setup()
     Serial.println("Booting");
 
     pixels.begin();
-    for (int i=0; i<NUMPIXELS; i+=2)
-        pixels.setPixelColor(i, pixels.Color(150,0,0));
+    for (int i=0; i<NUMPIXELS; i++)
+        pixels.setPixelColor(i, pixels.Color(30,0,0));
     pixels.show(); // This sends the updated pixel color to the hardware.
 
     wifiHelper.setupWifi();
@@ -53,12 +58,26 @@ void setup()
 
 void loop() {
 
-    for (int i=0; i< 12; i++) {
-        pixels.setPixelColor(i, pixels.Color(0,0,0));
-    }
-    pixels.show();
-    
     ArduinoOTA.handle();
+
+    button.serviceEvents();
 }
 
+void listener_Button(int eventCode, int eventParams) {
+
+    switch (eventParams) {
+        
+        case button.EV_BUTTON_PRESSED:
+            Serial.println("EV_BUTTON_PRESSED");
+            break;          
+        
+        case button.EV_HELD_FOR_LONG_ENOUGH:
+            Serial.println("EV_HELD_FOR_LONG_ENOUGH");
+            break;
+        
+        case button.EV_RELEASED:
+            Serial.println("EV_RELEASED");
+            break;
+    }
+}
 
