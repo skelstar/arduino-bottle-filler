890a6c220a41f9111feedcf519a9c9c4ec488da2
skelstar <skelstar@gmail.com>
2017-01-18 20:56:18 +1300

    7 Seg display wokring with MQTT service.

--- arduino-bottle-filler/arduino-bottle-filler.ino
+++ arduino-bottle-filler/arduino-bottle-filler.ino
@@ -3,8 +3,8 @@
 #include <myWifiHelper.h>
 #include <Adafruit_NeoPixel.h>
 #include <myPushButton.h>
-#include <TM1637.h>     // 7 Segment display
-
+#include <SevenSegmentTM1637.h>
+#include <SevenSegmentExtended.h>
 
 /* ----------------------------------------------------------- */
 
@@ -20,7 +20,7 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ40
 
 void listener_Button(int eventCode, int eventParams);
 
-myPushButton button(BUTTON_PIN, true, 2000, 1, listener_Button);
+myPushButton button(BUTTON_PIN, true, 1000, 1, listener_Button);
 
 // WIFI
 /* ----------------------------------------------------------- */
@@ -32,20 +32,58 @@ void wifiMsgCallback(char* message) {
     Serial.println(message);
 }
 
+#define FEEDBOTTLE_FEED  "dev/bottleFeed"
+
 // CLOCK
+
+#define MED_BRIGHT      30
+#define LOW_BRIGHT      15
+
 int8_t TimeDisp[] = {0x01,0x02,0x03,0x04};
-// unsigned char ClockPoint = 1;
-// unsigned char Update;
-// unsigned char halfsecond = 0;
-// unsigned char second;
-// unsigned char minute = 0;
-// unsigned char hour = 12;
 
 #define CLK 5   // SCL
 #define DIO 4   // SDA
-TM1637 tm1637(CLK,DIO);
+SevenSegmentExtended tm1637(CLK,DIO);
+
+long lastTimeRead = millis();
 
 /* ----------------------------------------------------------- */
+
+void devtime_callback(byte* payload, unsigned int length) {
+
+    if (payload[4] <= '9') {
+        int hour = (payload[0]-'0') * 10;
+        hour += payload[1]-'0';
+        int minute = (payload[3]-'0') * 10;
+        minute += payload[4]-'0';
+        tm1637.setBacklight(MED_BRIGHT);
+        tm1637.printTime(hour, minute, false);
+    } else {
+        char msg[5];
+        msg[0] = payload[0];
+        msg[1] = payload[1];
+        msg[2] = payload[2];
+        msg[3] = payload[3];
+        msg[4] = NULL;
+        tm1637.print(msg);
+    }
+
+    pinMode(0, OUTPUT);
+    digitalWrite(0, LOW);
+    delay(100);
+    digitalWrite(0, HIGH);
+
+    lastTimeRead = millis();
+}
+
+bool mqttTimeOnline() {
+    bool online =  lastTimeRead + 65000 > millis();
+    if (online == false) {
+        Serial.print("lastTimeRead: "); Serial.print(lastTimeRead); Serial.print(" millis(): "); Serial.println(millis());
+    }
+    return online;
+} 
+
 void setup() 
 {
     Serial.begin(9600);
@@ -57,23 +95,27 @@ void setup()
         pixels.setPixelColor(i, pixels.Color(30,0,0));
     pixels.show(); // This sends the updated pixel color to the hardware.
 
-    tm1637.set();
-    tm1637.init();
+    tm1637.begin();          
+    tm1637.setBacklight(LOW_BRIGHT);
+    tm1637.print("----");
 
     wifiHelper.setupWifi();
 
     wifiHelper.setupOTA("arduino-bottle-filler2");
+
+    wifiHelper.setupMqtt();
+    wifiHelper.mqttAddSubscription("dev/bottle-feed-time", devtime_callback);
 }
 
 /* ----------------------------------------------------------- */
 
 void loop() {
 
+    wifiHelper.loopMqtt();
+
     ArduinoOTA.handle();
 
     button.serviceEvents();
-
-    tm1637.display(TimeDisp);
     delay(100);
 }
 
@@ -82,15 +124,23 @@ void listener_Button(int eventCode, int eventParams) {
     switch (eventParams) {
         
         case button.EV_BUTTON_PRESSED:
+            wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_BUTTON_PRESSED");
             Serial.println("EV_BUTTON_PRESSED");
             break;          
         
         case button.EV_HELD_FOR_LONG_ENOUGH:
+            wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_HELD_FOR_LONG_ENOUGH");
             Serial.println("EV_HELD_FOR_LONG_ENOUGH");
             break;
         
         case button.EV_RELEASED:
+            wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_RELEASED");
             Serial.println("EV_RELEASED");
+            break;
+        
+        case button.EV_DOUBLETAP:
+            wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_DOUBLETAP");
+            Serial.println("EV_DOUBLETAP");
             break;
     }
 }
