01937661acbc3265a2cd3c3fa01d489336ccde85
skelstar <skelstar@gmail.com>
2017-01-29 12:38:55 +1300

    Got TaskScheduler working. Not implemented yet.

--- arduino-bottle-filler/arduino-bottle-filler.ino
+++ arduino-bottle-filler/arduino-bottle-filler.ino
@@ -1,22 +1,53 @@
 
-#include "paj7620.h"
 #include <myWifiHelper.h>
 #include <Adafruit_NeoPixel.h>
 #include <myPushButton.h>
 #include <SevenSegmentTM1637.h>
 #include <SevenSegmentExtended.h>
+#include <TaskScheduler.h>
+
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+
+/* ----------------------------------------------------------- */
+
+void t1Callback();
+
+Scheduler runner;
+
+Task t1(200, 2, &t1Callback, &runner, false);
+
+void t1Callback() {
+    Serial.print("t1: ");
+    Serial.println(millis());
+
+    if (t1.isFirstIteration()) {
+        Serial.println("First Iteration");
+    }
+    if (t1.isLastIteration()) {
+      Serial.println("Last Iteration");
+    }
+
+}
 
 /* ----------------------------------------------------------- */
 
 // NEOPIXEL
-#define PIN            15
-#define NUMPIXELS      12
+#define PIN            2
+#define NUMPIXELS      1
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ400);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(4*4, PIN, NEO_GRB + NEO_KHZ800);
 
+uint32_t COLOR_BUTTON_PRESSED = pixels.Color(0, 100, 0);
+uint32_t COLOR_NORMAL = pixels.Color(100, 0, 0);
+uint32_t COLOR_DELETED = pixels.Color(255, 0, 0);
+
+/* ----------------------------------------------------------- */
 
 // BUTTON
-#define BUTTON_PIN  14
+#define BUTTON_PIN  13
 
 void listener_Button(int eventCode, int eventParams);
 
@@ -24,16 +55,12 @@ myPushButton button(BUTTON_PIN, true, 1000, 1, listener_Button);
 
 // WIFI
 /* ----------------------------------------------------------- */
-void wifiMsgCallback(char* message);
 
-MyWifiHelper wifiHelper(wifiMsgCallback);
-
-void wifiMsgCallback(char* message) {
-    Serial.println(message);
-}
+MyWifiHelper wifiHelper("arduino-bottle-filler-dev");
 
 #define FEEDBOTTLE_FEED  "dev/bottleFeed"
 
+/* ----------------------------------------------------------- */
 // CLOCK
 
 #define MED_BRIGHT      30
@@ -43,46 +70,46 @@ int8_t TimeDisp[] = {0x01,0x02,0x03,0x04};
 
 #define CLK 5   // SCL
 #define DIO 4   // SDA
-SevenSegmentExtended tm1637(CLK,DIO);
+SevenSegmentExtended sevenSeg(CLK,DIO);
 
 long lastTimeRead = millis();
 
 /* ----------------------------------------------------------- */
 
-void devtime_callback(byte* payload, unsigned int length) {
+// void devtime_callback(byte* payload, unsigned int length) {
 
-    if (payload[4] <= '9') {
-        int hour = (payload[0]-'0') * 10;
-        hour += payload[1]-'0';
-        int minute = (payload[3]-'0') * 10;
-        minute += payload[4]-'0';
-        tm1637.setBacklight(MED_BRIGHT);
-        tm1637.printTime(hour, minute, false);
-    } else {
-        char msg[5];
-        msg[0] = payload[0];
-        msg[1] = payload[1];
-        msg[2] = payload[2];
-        msg[3] = payload[3];
-        msg[4] = NULL;
-        tm1637.print(msg);
-    }
+//     if (payload[4] <= '9') {
+//         int hour = (payload[0]-'0') * 10;
+//         hour += payload[1]-'0';
+//         int minute = (payload[3]-'0') * 10;
+//         minute += payload[4]-'0';
+//         sevenSeg.setBacklight(MED_BRIGHT);
+//         sevenSeg.printTime(hour, minute, false);
+//     } else {
+//         char msg[5];
+//         msg[0] = payload[0];
+//         msg[1] = payload[1];
+//         msg[2] = payload[2];
+//         msg[3] = payload[3];
+//         msg[4] = NULL;
+//         sevenSeg.print(msg);
+//     }
 
-    pinMode(0, OUTPUT);
-    digitalWrite(0, LOW);
-    delay(100);
-    digitalWrite(0, HIGH);
+//     pinMode(0, OUTPUT);
+//     digitalWrite(0, LOW);
+//     delay(100);
+//     digitalWrite(0, HIGH);
 
-    lastTimeRead = millis();
-}
+//     lastTimeRead = millis();
+// }
 
-bool mqttTimeOnline() {
-    bool online =  lastTimeRead + 65000 > millis();
-    if (online == false) {
-        Serial.print("lastTimeRead: "); Serial.print(lastTimeRead); Serial.print(" millis(): "); Serial.println(millis());
-    }
-    return online;
-} 
+// bool mqttTimeOnline() {
+//     bool online =  lastTimeRead + 65000 > millis();
+//     if (online == false) {
+//         Serial.print("lastTimeRead: "); Serial.print(lastTimeRead); Serial.print(" millis(): "); Serial.println(millis());
+//     }
+//     return online;
+// } 
 
 void setup() 
 {
@@ -91,32 +118,40 @@ void setup()
     Serial.println("Booting");
 
     pixels.begin();
-    for (int i=0; i<NUMPIXELS; i++)
-        pixels.setPixelColor(i, pixels.Color(30,0,0));
     pixels.show(); // This sends the updated pixel color to the hardware.
 
-    tm1637.begin();          
-    tm1637.setBacklight(LOW_BRIGHT);
-    tm1637.print("----");
+    pixels.setPixelColor(0, COLOR_NORMAL);
+    pixels.show();
+
+    // sevenSeg.begin();          
+    // sevenSeg.setBacklight(LOW_BRIGHT);
+    // sevenSeg.print("----");
 
     wifiHelper.setupWifi();
 
-    wifiHelper.setupOTA("arduino-bottle-filler2");
+    wifiHelper.setupOTA("arduino-bottle-filler-dev");
 
-    wifiHelper.setupMqtt();
-    wifiHelper.mqttAddSubscription("dev/bottle-feed-time", devtime_callback);
+    runner.startNow();
+    Serial.println("Initialized scheduler");
+
+    //wifiHelper.setupMqtt();
+
+    // wifiHelper.mqttAddSubscription("dev/bottle-feed-time", devtime_callback);
 }
 
 /* ----------------------------------------------------------- */
 
 void loop() {
 
-    wifiHelper.loopMqtt();
+    //wifiHelper.loopMqtt();
 
     ArduinoOTA.handle();
 
     button.serviceEvents();
-    delay(100);
+
+    runner.execute();
+    
+    //delay(100);
 }
 
 void listener_Button(int eventCode, int eventParams) {
@@ -124,22 +159,36 @@ void listener_Button(int eventCode, int eventParams) {
     switch (eventParams) {
         
         case button.EV_BUTTON_PRESSED:
-            wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_BUTTON_PRESSED");
+            //wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_BUTTON_PRESSED");
+            pixels.setPixelColor(0, COLOR_BUTTON_PRESSED);
+            pixels.show();
             Serial.println("EV_BUTTON_PRESSED");
             break;          
         
         case button.EV_HELD_FOR_LONG_ENOUGH:
-            wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_HELD_FOR_LONG_ENOUGH");
+            //wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_HELD_FOR_LONG_ENOUGH");
             Serial.println("EV_HELD_FOR_LONG_ENOUGH");
+            pixels.setPixelColor(0, COLOR_DELETED);
+            pixels.show();
+
+            //runner.addTask(t1);
+            t1.restart();
+            Serial.println("Restarted t1");
+
             break;
         
         case button.EV_RELEASED:
-            wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_RELEASED");
+            //wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_RELEASED");
             Serial.println("EV_RELEASED");
+            pixels.setPixelColor(0, COLOR_NORMAL);
+            pixels.show();
+
+            // Serial.println("Disabled and deleted t1");
+            
             break;
         
         case button.EV_DOUBLETAP:
-            wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_DOUBLETAP");
+            //wifiHelper.mqttPublish(FEEDBOTTLE_FEED, "EV_DOUBLETAP");
             Serial.println("EV_DOUBLETAP");
             break;
     }
